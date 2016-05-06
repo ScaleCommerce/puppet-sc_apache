3f12629489199be2065399002b717013419977d8
Andreas Ziethen <az@scale.sc>
2016-05-06 14:44:41 +0200

    move main apache stuff to init.pp

--- manifests/vhosts.pp
+++ manifests/vhosts.pp
@@ -33,54 +33,9 @@
 #
 class sc_apache::vhosts (
   $vhosts = {},
-  $supervisor_init_script = '/etc/supervisor/supervisor-init-wrapper',
-  $supervisor_conf_script = '/etc/supervisor/conf.d/apache2.conf',
 ){
 
-  include apache
-  include apt
-  include apache::mod::php
-  include apache::mod::rewrite
-  include apache::mod::setenvif
-  include apache::mod::auth_basic
-  include apache::mod::deflate
-  include apache::mod::expires
-  include apache::mod::headers
-  include apache::mod::remoteip
-  include apache::mod::status
-  include apache::mod::authz_user
-  include apache::mod::alias
-  include apache::mod::authn_core
-  include apache::mod::authn_file
-  include apache::mod::reqtimeout
-  include apache::mod::negotiation
-  include apache::mod::autoindex
 
-  ::apache::mod { 'access_compat': }
-
-  # supervisor
-  if $::virtual == 'docker' {
-    file { '/etc/init/apache2.conf':
-      ensure => absent,
-    }->
-    file { '/etc/init.d/apache2':
-      ensure => link,
-      target => $supervisor_init_script,
-    }
-
-    file { $supervisor_conf_script:
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0644',
-      content => template("${module_name}/apache.supervisor.conf.erb"),
-      notify => Exec['supervisorctl_update'],
-    }
-
-    exec {'supervisorctl_update':
-      command => '/usr/bin/supervisorctl update',
-      refreshonly => true,
-    }
-  }
 
   $vhost_defaults = hiera_hash('apache::vhosts_defaults', {})
 
