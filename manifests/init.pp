a970fe000933797f23865cbbb44b1fb8472c183e
Andreas Ziethen <az@scale.sc>
2016-11-22 16:30:04 +0100

    add old version in else clause

--- manifests/init.pp
+++ manifests/init.pp
@@ -52,6 +52,26 @@ class sc_apache (
         notify  => Service['apache2'],
       }
     }
+  } else {
+    include apache::mod::rewrite
+    include apache::mod::setenvif
+    include apache::mod::auth_basic
+    include apache::mod::deflate
+    include apache::mod::expires
+    include apache::mod::headers
+    include apache::mod::remoteip
+    include apache::mod::status
+    include apache::mod::authz_user
+    include apache::mod::alias
+    include apache::mod::authn_core
+    include apache::mod::authn_file
+    include apache::mod::reqtimeout
+    include apache::mod::negotiation
+    include apache::mod::autoindex
+
+    ::apache::mod { 'access_compat': }
+    ::apache::mod { 'env': }
+    ::apache::mod { 'authz_groupfile': }
   }
 
 
