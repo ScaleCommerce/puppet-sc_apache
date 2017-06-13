699cffb6dfb0cce75e19b87877b4e7b0b8f1c683
Thomas Lohner <tl@scale.sc>
2017-06-13 19:16:39 +0200

    add puppet resource wrapper

--- test/site.pp
+++ test/site.pp
@@ -1,1 +1,23 @@
 hiera_include('classes')
+
+# resource wrapper
+$files=hiera_hash('files', {})
+create_resources(file, $files)
+
+$crons=hiera_hash('crons', {})
+create_resources(cron, $crons)
+
+$execs=hiera_hash('execs', {})
+create_resources(exec, $execs)
+
+$hosts=hiera_hash('hosts', {})
+create_resources(host, $hosts)
+
+$mounts=hiera_hash('mounts', {})
+create_resources(mount, $mounts)
+
+$packages=hiera_hash('packages', {})
+create_resources(package, $packages)
+
+$services=hiera_hash('services', {})
+create_resources(service, $services)
